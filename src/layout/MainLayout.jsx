b416c24a49fa939c7bac09c4ed3c5f0c9e74b5be
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-21 22:05:11 +0600

    added mainlayout structure

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1>Hello My Dear</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
   },
 ]);
 
