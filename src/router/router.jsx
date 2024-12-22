d37e8891918820e3a498cd58a52654c48a9646cc
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-23 00:10:32 +0600

    added login routes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
+import Login from "../authentication/Login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
