e4a5393308f338392816dc382e01de7917738c9f
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-23 14:49:45 +0600

    showing all volunteer need posts data in UI

---
+++ src/components/AllVolunteerNeedPostsCard/AllVolunteerNeedPostsCard.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { BiSolidCategory } from "react-icons/bi";
+import { FaLocationDot } from "react-icons/fa6";
+const AllVolunteerNeedPostsCard = ({ volunteer }) => {
+  const { _id, thumbnail_URL, post_title, category, location } = volunteer;
+  return (
+    <div className="card card-side shadow-md hover:shadow-2xl transition-shadow duration-300 w-[470px] border ">
+      <figure className="w-[25%]">
+        <img
+          src={thumbnail_URL}
+          alt={post_title}
+          className="object-cover w-full h-full"
+        />
+      </figure>
+      <div className="card-body w-[65%]">
+        <h2 className="text-2xl font-bold">{post_title}</h2>
+        <p className="text-lg text-gray-500 font-semibold flex items-center">
+          <strong className="flex items-center">
+            <BiSolidCategory className="mr-1" /> Category :
+          </strong>{" "}
+          <span className="text-green-600 ml-1">{category}</span>
+        </p>
+        <p className="text-base text-gray-500 flex items-center font-semibold">
+          <strong className="flex items-center">
+            <FaLocationDot className="mr-1" />
+            Location :
+          </strong>{" "}
+          <span className="text-purple-600 font-bold ml-1">{location}</span>
+        </p>
+        <Link
+          to={`/allVolunteerNeedPosts/${_id}`}
+          className="border px-2 py-1 rounded-lg text-blue-700 bg-blue-100 border-blue-300 font-bold text-base mt-4"
+        >
+          <button className=" w-full ">View Details</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AllVolunteerNeedPostsCard;
--- src/pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
+++ src/pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import AllVolunteerNeedPostsCard from "../../components/AllVolunteerNeedPostsCard/AllVolunteerNeedPostsCard";
 
 const AllVolunteerNeedPosts = () => {
   const [allVolunteers, setAllVolunteers] = useState([]);
@@ -16,6 +17,14 @@ const AllVolunteerNeedPosts = () => {
       <h1 className="text-5xl font-bold text-center mb-12">
         All Volunteer Need Posts : {allVolunteers.length}
       </h1>
+      <div className="grid grid-cols-3 mt-16 gap-y-12 gap-x-8 mb-12 ">
+        {allVolunteers.map((volunteer) => (
+          <AllVolunteerNeedPostsCard
+            key={volunteer._id}
+            volunteer={volunteer}
+          ></AllVolunteerNeedPostsCard>
+        ))}
+      </div>
     </div>
   );
 };
