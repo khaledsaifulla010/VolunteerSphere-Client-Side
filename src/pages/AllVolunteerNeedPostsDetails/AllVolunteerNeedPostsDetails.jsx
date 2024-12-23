cf4e7e29ba689c04bac5f6f4fc1f3be42a98551e
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-23 23:11:24 +0600

    showing a single volunteer need posts data on UI

--- src/pages/AllVolunteerNeedPostsDetails/AllVolunteerNeedPostsDetails.jsx
+++ src/pages/AllVolunteerNeedPostsDetails/AllVolunteerNeedPostsDetails.jsx
@@ -1,5 +1,12 @@
 import { useLoaderData } from "react-router-dom";
-
+import { BiSolidCategory } from "react-icons/bi";
+import { GrUserWorker } from "react-icons/gr";
+import { IoLocation } from "react-icons/io5";
+import { FaCalendarDays } from "react-icons/fa6";
+import { FaBuilding } from "react-icons/fa";
+import { MdAttachEmail } from "react-icons/md";
+import { HiDocumentCheck } from "react-icons/hi2";
+import { FaCertificate } from "react-icons/fa";
 const AllVolunteerNeedPostsDetails = () => {
   const postsDetails = useLoaderData();
 
@@ -16,51 +23,84 @@ const AllVolunteerNeedPostsDetails = () => {
   } = postsDetails;
 
   return (
-    <div className="mt-12">
+    <div className="mt-12 font-5">
       <h1 className="text-5xl font-bold text-center mb-12">
-        All Volunteer Need Posts Details
+        "{post_title}" Posts Details
       </h1>
-      <div className="flex justify-center items-center p-8 mt-12 mb-24">
-        <div className="card bg-slate-100 border-2 border-gray-300 w-[1200px] h-[550px] shadow-lg transition-transform duration-500 ease-in-out transform hover:scale-105">
+      <div className=" flex justify-center items-center p-8 mt-12 mb-24">
+        <div className="card bg-gray-50 border-2 border-gray-300 w-[1200px] h-[700px] shadow-lg transition-transform duration-500 ease-in-out transform hover:scale-105">
           <figure className="flex">
             <img
               src={thumbnail_URL}
               alt="Post Thumbnail"
-              className="object-cover w-[40%] h-[550px] border-r"
+              className="object-cover w-[40%] h-[700px] border-r"
             />
-            <div className="flex flex-col p-6 w-[60%]">
-              <h2 className="text-4xl font-bold text-indigo-800 bg-indigo-50 border-indigo-200 border p-2 rounded-xl w-full text-center">
+            <div className="flex flex-col p-4 w-[60%]">
+              <h2 className="text-4xl font-bold text-teal-800 bg-teal-50 border-teal-200 border p-2 rounded-xl w-full text-center ">
                 {post_title}
               </h2>
               <div className="mt-10 text-lg text-gray-700 flex justify-between gap-8">
-                <p className="text-green-600 bg-green-50 border-green-200 border p-2 rounded-xl w-[40%] text-center">
-                  <strong>Category: </strong> {category}
+                <p
+                  className="text-green-600 bg-green-50 border-green-200 border p-2 rounded-xl 
+                  w-full flex items-center justify-center
+                font-semibold"
+                >
+                  <BiSolidCategory className="mr-1" /> Category : {category}
                 </p>
-                <p className="text-green-600 bg-green-50 border-green-200 border p-2 rounded-xl w-[60%] text-center">
-                  <strong>Location: </strong> {location}
+                <p
+                  className="text-green-600 bg-green-50 border-green-200 border p-2 rounded-xl 
+                  w-full flex items-center justify-center
+                font-semibold"
+                >
+                  <GrUserWorker className="mr-1" /> Volunteers Needed :{" "}
+                  {volunteersNeeded}
                 </p>
               </div>
               <div className="mt-8 text-lg text-gray-700 flex justify-between gap-8">
-                <p className="text-red-600 bg-red-50 border-red-200 border p-2 rounded-xl w-[40%] text-center">
-                  <strong>Volunteers Needed: </strong> {volunteersNeeded}
+                <p
+                  className="text-red-600 bg-red-50 border-red-200 border p-2 rounded-xl
+                  w-full flex items-center justify-center
+                font-semibold"
+                >
+                  <IoLocation className="mr-1" /> Location : {location}
                 </p>
-                <p className="text-red-600 bg-red-50 border-red-200 border p-2 rounded-xl w-[60%] text-center">
-                  <strong>Deadline: </strong> {deadline}
+                <p
+                  className="text-red-600 bg-red-50 border-red-200 border p-2 rounded-xl
+                   w-full flex items-center justify-center
+                font-semibold"
+                >
+                  <FaCalendarDays className="mr-1" /> Deadline : {deadline}
                 </p>
               </div>
               <div className="mt-8 text-lg text-gray-700 flex justify-between gap-8">
-                <p className="text-blue-600 bg-blue-50 border-blue-200 border p-2 rounded-xl w-[40%] text-center">
-                  <strong>Organizer Name: </strong> {organizerName}
+                <p
+                  className="text-blue-600 bg-blue-50 border-blue-200 border p-2 rounded-xl
+                   w-full flex items-center justify-center
+                font-semibold"
+                >
+                  <FaBuilding className="mr-1" /> Organizer Name :{" "}
+                  {organizerName}
                 </p>
-                <p className="text-blue-600 bg-blue-50 border-blue-200 border p-2 rounded-xl w-[60%] text-center">
-                  <strong>Organizer Email: </strong> {organizerEmail}
+                <p
+                  className="text-blue-600 bg-blue-50 border-blue-200 border p-2 rounded-xl
+                   w-full flex items-center justify-center
+                font-semibold"
+                >
+                  <MdAttachEmail className="mr-1 mt-0.5" /> Organizer Email :{" "}
+                  {organizerEmail}
                 </p>
               </div>
               <div className="divider mt-6"></div>
-              <p className="mt-4 text-gray-600 text-lg font-bold text-justify">
-                <strong className="text-orange-600">Description: </strong>
+              <p className="mt-4 text-gray-600 text-lg border p-4 rounded-xl bg-orange-50 border-orange-300   text-justify  h-[200px]">
+                <p className="flex text-orange-600 font-bold">
+                  <HiDocumentCheck className="mr-1 mt-1" />{" "}
+                  <span className="underline mb-1">Description</span> :{" "}
+                </p>
                 {description}
               </p>
+              <button className="border px-2 py-1 rounded-lg text-purple-800 bg-purple-100 border-purple-300 font-bold text-lg mt-10 flex items-center justify-center gap-1">
+                <FaCertificate /> Be a Volunteer
+              </button>
             </div>
           </figure>
         </div>
