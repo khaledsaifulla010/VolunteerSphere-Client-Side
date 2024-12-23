bf973ea09947e19fb515c20d909172b33671174b
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-23 14:30:54 +0600

    load all volunteer need posts data

--- src/pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
+++ src/pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
@@ -1,11 +1,23 @@
-
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const AllVolunteerNeedPosts = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [allVolunteers, setAllVolunteers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/allVolunteerNeedsPosts")
+      .then((data) => setAllVolunteers(data.data));
+  }, []);
+
+  return (
+    <div className="mt-12 font-4">
+      <h1 className="text-5xl font-bold text-center mb-12">
+        All Volunteer Need Posts : {allVolunteers.length}
+      </h1>
+    </div>
+  );
 };
 
 export default AllVolunteerNeedPosts;
