7eb587efc7f54c9614b187f21fb286a9611834df
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-25 01:03:17 +0600

    added toggle layout in all volunteers needs posts page

--- src/components/AllVolunteerNeedPostsCard/AllVolunteerNeedPostsCard.jsx
+++ src/components/AllVolunteerNeedPostsCard/AllVolunteerNeedPostsCard.jsx
@@ -1,10 +1,42 @@
 import { Link } from "react-router-dom";
-import { BiSolidCategory } from "react-icons/bi";
-import { FaLocationDot } from "react-icons/fa6";
-const AllVolunteerNeedPostsCard = ({ volunteer }) => {
-  const { _id, thumbnail_URL, post_title, category, location } = volunteer;
+
+const AllVolunteerNeedPostsCard = ({ volunteer, isTableLayout }) => {
+  const { _id, thumbnail_URL, post_title, location } = volunteer;
+
+  if (isTableLayout) {
+    return (
+      <tr className="hover:bg-gray-100">
+        <td className="text-center py-3 px-4">
+          <div className="avatar flex justify-center">
+            <div className="mask rounded-full h-16 w-16">
+              <img
+                src={thumbnail_URL}
+                alt={`${post_title} Thumbnail`}
+                className="object-cover"
+              />
+            </div>
+          </div>
+        </td>
+        <td className="text-center py-3 px-4 text-base text-purple-700 font-bold">
+          {post_title}
+        </td>
+        <td className="text-center py-3 px-4 text-indigo-600 font-bold">
+          {location}
+        </td>
+        <td className="text-center py-3 px-4">
+          <Link
+            to={`/allVolunteerNeedsPosts/${_id}`}
+            className="border px-2 py-1 rounded-lg text-blue-700 bg-blue-100 border-blue-300 font-bold text-base mt-4"
+          >
+            View Details
+          </Link>
+        </td>
+      </tr>
+    );
+  }
+
   return (
-    <div className="card card-side shadow-md hover:shadow-2xl transition-shadow duration-300 w-[470px] border ">
+    <div className="card card-side shadow-md hover:shadow-2xl transition-shadow duration-300 w-[480px] border">
       <figure className="w-[25%]">
         <img
           src={thumbnail_URL}
@@ -14,24 +46,15 @@ const AllVolunteerNeedPostsCard = ({ volunteer }) => {
       </figure>
       <div className="card-body w-[65%]">
         <h2 className="text-2xl font-bold">{post_title}</h2>
-        <p className="text-lg text-gray-500 font-semibold flex items-center">
-          <strong className="flex items-center">
-            <BiSolidCategory className="mr-1" /> Category :
-          </strong>{" "}
-          <span className="text-green-600 ml-1">{category}</span>
-        </p>
         <p className="text-base text-gray-500 flex items-center font-semibold">
-          <strong className="flex items-center">
-            <FaLocationDot className="mr-1" />
-            Location :
-          </strong>{" "}
+          <strong className="flex items-center">Location: </strong>
           <span className="text-purple-600 font-bold ml-1">{location}</span>
         </p>
         <Link
           to={`/allVolunteerNeedsPosts/${_id}`}
-          className="border px-2 py-1 rounded-lg text-blue-700 bg-blue-100 border-blue-300 font-bold text-base mt-4"
+          className="border px-2 py-1 rounded-lg text-blue-700 bg-blue-100 border-blue-300 font-bold text-base mt-4 text-center"
         >
-          <button className=" w-full ">View Details</button>
+          View Details
         </Link>
       </div>
     </div>
--- src/pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
+++ src/pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import AllVolunteerNeedPostsCard from "../../components/AllVolunteerNeedPostsCard/AllVolunteerNeedPostsCard";
-
+import { IoGrid } from "react-icons/io5";
+import { RiTableView } from "react-icons/ri";
 const AllVolunteerNeedPosts = () => {
   const [allVolunteers, setAllVolunteers] = useState([]);
+  const [isTableLayout, setIsTableLayout] = useState(false);
 
   useEffect(() => {
     axios
@@ -12,19 +13,74 @@ const AllVolunteerNeedPosts = () => {
       .then((data) => setAllVolunteers(data.data));
   }, []);
 
+  const handleLayoutToggle = (layout) => {
+    setIsTableLayout(layout);
+  };
+
   return (
-    <div className="mt-12 font-4">
-      <h1 className="text-5xl font-bold text-center mb-12">
-        All Volunteer Need Posts : {allVolunteers.length}
+    <div className="mt-12 font-4 mb-24">
+      <h1 className="text-5xl font-bold text-center mb-2">
+        All Volunteer Need Posts
       </h1>
-      <div className="grid grid-cols-3 mt-16 gap-y-12 gap-x-8 mb-12 ">
-        {allVolunteers.map((volunteer) => (
-          <AllVolunteerNeedPostsCard
-            key={volunteer._id}
-            volunteer={volunteer}
-          ></AllVolunteerNeedPostsCard>
-        ))}
+      <div className="text-center mb-2 flex items-center justify-end gap-4">
+        <div className="tooltip font-bold" data-tip="Card View">
+          <button
+            onClick={() => handleLayoutToggle(false)}
+            className={`px-4 py-2 ${
+              isTableLayout ? "bg-gray-500" : "bg-blue-500"
+            } text-white rounded-lg mx-2`}
+          >
+            <IoGrid />
+          </button>
+        </div>
+        <div className="tooltip font-bold" data-tip="Table View">
+          <button
+            onClick={() => handleLayoutToggle(true)}
+            className={`px-4 py-2 ${
+              !isTableLayout ? "bg-gray-500" : "bg-blue-500"
+            } text-white rounded-lg mx-2`}
+          >
+            <RiTableView />
+          </button>
+        </div>
       </div>
+      <div className="divider"></div>
+      {isTableLayout ? (
+        <div className="overflow-x-auto mt-20 mb-72 px-8">
+          <div className="rounded-lg shadow-lg border border-gray-300">
+            <table className="table w-full border-collapse">
+              <thead className="bg-gray-700 text-white text-lg font-extrabold">
+                <tr>
+                  <th className="text-center py-3 px-4">Thumbnail</th>
+                  <th className="text-center py-3 px-4">Post Title</th>
+                  <th className="text-center py-3 px-4">Location</th>
+                  <th className="text-center py-3 px-4">View Details</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-700">
+                {allVolunteers.map((volunteer, index) => (
+                  <AllVolunteerNeedPostsCard
+                    key={volunteer._id}
+                    volunteer={volunteer}
+                    index={index}
+                    isTableLayout={isTableLayout}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : (
+        <div className="grid grid-cols-3 mt-16 gap-y-12 gap-x-8 mb-12">
+          {allVolunteers.map((volunteer) => (
+            <AllVolunteerNeedPostsCard
+              key={volunteer._id}
+              volunteer={volunteer}
+              isTableLayout={isTableLayout}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
