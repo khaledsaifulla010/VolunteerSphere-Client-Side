f817b01e9e36cdb7fd7269267ea11744fa362f3b
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-24 18:32:54 +0600

    load all volunteer needs post data indibidually using email query

--- src/pages/ManageMyPosts/ManageMyPosts.jsx
+++ src/pages/ManageMyPosts/ManageMyPosts.jsx
@@ -1,4 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "../../hooks/useAuth";
 const ManageMyPosts = () => {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:5000/allVolunteerNeedsPostsIndividually?email=${user.email}`
+      )
+      .then((data) => console.log(data.data));
+  }, [user.email]);
+
   return (
     <div className="mt-12 font-2">
       <h1 className="text-5xl font-bold text-center mb-12">Manage My Posts</h1>
