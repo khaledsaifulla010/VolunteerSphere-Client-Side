f5b22b76f987e330cbe1999368c1c9e3096d209a
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-24 19:36:29 +0600

    showing all volunteer needs post data indibidually using email query in UI

---
+++ src/components/ManageMyVolunteerNeedPosts/ManageMyVolunteerNeedPosts.jsx
@@ -0,0 +1,94 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { FaEdit } from "react-icons/fa";
+import { RiDeleteBin2Fill } from "react-icons/ri";
+const ManageMyVolunteerNeedPosts = () => {
+  const { user } = useAuth();
+  const [volunteerPosts, setVolunteerPost] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:5000/allVolunteerNeedsPostsIndividually?email=${user.email}`
+      )
+      .then((data) => setVolunteerPost(data.data));
+  }, [user.email]);
+
+  return (
+    <div className="mt-12">
+      {volunteerPosts.length > 0 ? (
+        <div className="overflow-x-auto mt-20 mb-72 px-8">
+          <div className="rounded-lg shadow-lg border border-gray-300">
+            <table className="table w-full border-collapse">
+              <thead className="bg-gray-700 text-white text-lg font-extrabold">
+                <tr>
+                  <th className="text-center py-3 px-4">SL/No.</th>
+                  <th className="text-center py-3 px-4">Thumbnail</th>
+                  <th className="text-center py-3 px-4">Post Title</th>
+                  <th className="text-center py-3 px-4">Category</th>
+                  <th className="text-center py-3 px-4">Location</th>
+                  <th className="text-center py-3 px-4">Volunteers Needed</th>
+                  <th className="text-center py-3 px-4">Deadline</th>
+                  <th className="text-center py-3 px-4">Update</th>
+                  <th className="text-center py-3 px-4">Delete</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-700">
+                {volunteerPosts.map((post, index) => (
+                  <tr key={post._id} className="hover:bg-gray-100">
+                    <td className="text-center py-3 px-4 font-bold text-green-600 text-base">
+                      {index + 1}
+                    </td>
+                    <td className="text-center py-3 px-4">
+                      <div className="avatar flex justify-center">
+                        <div className="mask rounded-full h-16 w-16">
+                          <img
+                            src={post.thumbnail_URL}
+                            alt={`${post.post_title} Thumbnail`}
+                            className="object-cover"
+                          />
+                        </div>
+                      </div>
+                    </td>
+                    <td className="text-center py-3 px-4 text-base text-purple-700 font-bold">
+                      {post.post_title}
+                    </td>
+                    <td className="text-center py-3 px-4 text-green-700 font-bold text-base">
+                      {post.category}
+                    </td>
+                    <td className="text-center py-3 px-4 text-indigo-600 font-bold text-base">
+                      {post.location}
+                    </td>
+                    <td className="text-center py-3 px-4 text-orange-600 font-bold text-base">
+                      {post.volunteersNeeded}
+                    </td>
+                    <td className="text-center py-3 px-4 text-rose-600 font-bold text-base">
+                      {post.deadline}
+                    </td>
+                    <td className="text-center text-2xl py-3 px-4 text-amber-500 font-bold ">
+                      <button>
+                        <FaEdit />
+                      </button>
+                    </td>
+                    <td className="text-center py-3 px-4 text-red-600 font-bold text-2xl">
+                      <button>
+                        <RiDeleteBin2Fill />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : (
+        <h1 className="text-2xl font-bold text-slate-500 text-center mt-36 mb-36">
+          No Volunteer Need Posts Found!
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export default ManageMyVolunteerNeedPosts;
--- src/pages/ManageMyPosts/ManageMyPosts.jsx
+++ src/pages/ManageMyPosts/ManageMyPosts.jsx
@@ -1,20 +1,29 @@
-import axios from "axios";
-import { useEffect } from "react";
-import useAuth from "../../hooks/useAuth";
+import ManageMyVolunteerNeedPosts from "../../components/ManageMyVolunteerNeedPosts/ManageMyVolunteerNeedPosts";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import "./CustomTabsStyle.css";
 const ManageMyPosts = () => {
-  const { user } = useAuth();
-
-  useEffect(() => {
-    axios
-      .get(
-        `http://localhost:5000/allVolunteerNeedsPostsIndividually?email=${user.email}`
-      )
-      .then((data) => console.log(data.data));
-  }, [user.email]);
-
   return (
-    <div className="mt-12 font-2">
-      <h1 className="text-5xl font-bold text-center mb-12">Manage My Posts</h1>
+    <div className="mt-12 mb-24 font-2">
+      <h1 className="text-5xl font-bold text-center mb-16 ">Manage My Post</h1>
+      <Tabs>
+        <div className="border border-purple-200 p-4 bg-purple-100 rounded-xl">
+          <TabList className="custom-tab-list ">
+            <Tab className="custom-tab">My Volunteer Need Posts</Tab>
+            <h1>Hello</h1>
+            <Tab className="custom-tab">My Volunteer Request Post</Tab>
+          </TabList>
+        </div>
+
+        <div className="mt-24">
+          <TabPanel>
+            <div><ManageMyVolunteerNeedPosts></ManageMyVolunteerNeedPosts></div>
+          </TabPanel>
+          <TabPanel>
+            <div>Any Content 2</div>
+          </TabPanel>
+        </div>
+      </Tabs>
     </div>
   );
 };
